b2fb14d6a0d7b78742f2a23b2bc36d91dfeafb4f
Amelia Riely <amelia.riely@gmail.com>
2017-10-11 15:39:36 -0400

    Models generated from jsonschema2pojo

---
+++ app/src/main/java/com/ameliariely/newmacbookwhodis/Models/NYTResult.kt
@@ -0,0 +1,27 @@
+package com.ameliariely.newmacbookwhodis.Models
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class NYTResult {
+
+    @SerializedName("status")
+    @Expose
+    var status: String? = null
+    @SerializedName("copyright")
+    @Expose
+    var copyright: String? = null
+    @SerializedName("section")
+    @Expose
+    var section: String? = null
+    @SerializedName("last_updated")
+    @Expose
+    var lastUpdated: String? = null
+    @SerializedName("num_results")
+    @Expose
+    var numResults: Int? = null
+    @SerializedName("results")
+    @Expose
+    var results: List<NYTStory>? = null
+
+}
